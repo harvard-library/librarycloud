ebd5405a0412bf799787da61bceb98f522c14cdd
Michael Vandermillen <michael_vandermillen@harvard.edu>
2015-04-08 09:27:06 -0400

    adding ModsGroup

---
+++ src/edu/harvard/lib/lcloud/ModsGroup.java
@@ -0,0 +1,24 @@
+package edu.harvard.lib.lcloud;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+
+import gov.loc.mods.v3.ModsType;
+
+public class ModsGroup {
+	
+	private List<ModsType> items;
+	
+	public ModsGroup() {
+	}
+	
+	@XmlElement(name = "mods", namespace="http://www.loc.gov/mods/v3")
+	public List<ModsType> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<ModsType> items) {
+		this.items = items;
+	}	
+}
