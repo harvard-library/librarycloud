aa56639499a24b84267f421cde173b8a610dd1da
Michael Vandermillen <michael_vandermillen@harvard.edu>
2018-09-06 11:21:35 -0400

    add back priorRecordIdentifier clause, but add "sort" to subsequent non-query settings to ignore (already processed)

--- src/edu/harvard/lib/librarycloud/items/ItemDAO.java
+++ src/edu/harvard/lib/librarycloud/items/ItemDAO.java
@@ -404,7 +404,7 @@ public class ItemDAO {
 					if (value.contains(":"))
 						value = "\"" + value + "\"";
 					queryList.add("(recordIdentifier:" + value + " OR priorRecordIdentifier:" + value + ")");
-		} else if (key.equals("facet") || key.equals("facets") || key.equals("limit") || key.equals("start")) {
+		} else if (key.equals("facet") || key.equals("facets") || key.equals("limit") || key.equals("start") || key.startsWith("sort")) {
 		} else {
             if (key.endsWith("_exact") || key.equals("fileDeliveryURL") || key.equals("availableTo"))
 						queryList.add(key.replace("_exact", "") + ":\"" + value
@@ -423,7 +423,7 @@ public class ItemDAO {
               if (key.equals("seriesTitle")) {
                 key = "relatedItem";
               }
-							queryList.add(key + "_keyword:" + value);
+							queryList.add("(" + key + "_keyword:" + value + ")");
             }
 					}
 				}
