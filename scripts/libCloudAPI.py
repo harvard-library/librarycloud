f6f1b2181e47bb8d7b451db781860427ede54e6e
Michael Vandermillen <michael_vandermillen@harvard.edu>
2018-11-08 11:12:17 -0500

    new version python reporting script

--- scripts/libCloudAPI.py
+++ scripts/libCloudAPI.py
@@ -4,7 +4,16 @@
 import urllib.request 
 import xml.dom.minidom
 
-def printIDs(doc):
+def printIDs(doc):  
+  # check that the record exists in the API
+  # query = doc.getElementsByTagName("query")
+  numFound = doc.getElementsByTagName("numFound")
+  total = numFound[0].firstChild.nodeValue
+  if total == "0":
+    query = doc.getElementsByTagName("query")
+    query = query[0].firstChild.nodeValue
+    print ("NO_RECORDS_FOUND_FOR_QUERY:_" + query)
+  
   # get mods:mods records
   modsRecords = doc.getElementsByTagName("mods:mods")
   # loop through each mods:mods record
@@ -14,18 +23,17 @@ def printIDs(doc):
     # loop through each mods:recordIdentifier
     for recordIdentifier in recordIdentifiers:
       # return recordIdentifiers only with source of: VIA, ALEPH, or OASIS 
-      if recordIdentifier.getAttribute("source") in ("MH:VIA","MH:ALEPH","MH:OASIS"):
+      if recordIdentifier.getAttribute("source") in ("MH:VIA","MH:ALEPH","MH:OASIS","MH:ALMA"):
         # set recordIdentifier as first thing to print in results 
         results = recordIdentifier.firstChild.nodeValue
         # get typeOfResource elements
         collectionRecords = modsRecord.getElementsByTagName("mods:typeOfResource")
         # loop through typeOfResource elements to identify which are collection records
+        results = results + "\t"
         for collectionRecord in collectionRecords:
             # get typeOfResource elements with attribute collection="yes"
             if collectionRecord.getAttribute("collection") == "yes":
-              results = results + "\tCollection Record"
-            else: 
-              results = results + "\t"
+              results = results + "Collection_Record"
         # get mods:url elements
         modsURLs = modsRecord.getElementsByTagName("mods:url")
         rawObject = 0
@@ -35,21 +43,31 @@ def printIDs(doc):
           # check for raw objects, set to 1 if any found
           if modsURL.getAttribute("access") == "raw object":
             rawObject = 1
-          # check for previews, set to 1 if any found
-          if modsURL.getAttribute("access") == "preview":
+          # check for previews that are NOT empty, set to 1 if any found
+          if modsURL.getAttribute("access") == "preview" and modsURL.firstChild is not None:
             preview = 1
         # if no rawObject found:
+        results = results + "\t"
         if rawObject == 0:
-          # print "NOT DIGITIZED"
-          results = results + "\tNOT DIGITIZED"
+          # print "NO RAW OBJECT"
+          results = results + "NO_RAW_OBJECT"
         else: 
-          results = results + "\t"
+          results = results
         # if a rawObject is found but no preview is found:
-        if preview == 0 and rawObject == 1:
+        results = results + "\t"
+        if preview == 0:
           # print "NO THUMBNAIL"
-          results = results + "\tNO THUMBNAIL"
+          results = results + "NO_THUMBNAIL"
         else: 
-          results = results + "\t"
+          results = results
+
+        # get HarvardDRS:accessFlag
+        accessFlagCheck = "NO_VALUE_FOUND"
+        accessFlags = modsRecord.getElementsByTagName("HarvardDRS:accessFlag")
+        results = results + "\t"
+        for accessFlag in accessFlags:
+            accessFlagCheck = accessFlag.firstChild.nodeValue
+        results = results + accessFlagCheck 
           
         print (results)
 
@@ -59,6 +77,10 @@ def main():
   #     ALEPH: 9 digits with leading zeros
   #     VIA: Wxxxx_ or Sxxxx_ 
   f=open("harvestFile.txt", "r")
+
+  #print header row
+  print ("RecordID\tCollection?\tRaw?\tThumb?\tAccessFlag")
+
   if f.mode == 'r':
     content = f.readlines()
     # remove whitespace characters like `\n` at the end of each line
@@ -66,9 +88,14 @@ def main():
 
   for x in content:
     # define a variable to hold the source URL
-    urlData = "https://api.lib.harvard.edu/v2/items?"
+    urlData = "https://api-qa.lib.harvard.edu/v2/items?"
     # add recordIdentifier=ID* and increase limit to 250
-    urlData = urlData + "recordIdentifier=" + x + "*" + "&limit=250"
+    
+    # use for all records except JSTOR Forum URNs
+    # urlData = urlData + "recordIdentifier=" + x + "*" + "&limit=250"
+    
+    # use for JSTOR Forum URNs
+    urlData = urlData + "q=" + x + "&limit=250"
 
     # open the URL and read the data
     webUrl = urllib.request.urlopen(urlData)
@@ -78,6 +105,7 @@ def main():
     
     # put urlData in DOM
     doc = xml.dom.minidom.parse(urllib.request.urlopen(urlData))
+
     # print results
     printIDs (doc)
 
